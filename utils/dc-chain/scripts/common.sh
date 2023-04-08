2e775c413cf4e6cc15630b9fc53eba65de9718a7
Colton Pawielski <cepawiel@mtu.edu>
2023-04-08 16:33:24 -0500

    Add ability to specify git repos for dc-chain sources

--- utils/dc-chain/scripts/common.sh
+++ utils/dc-chain/scripts/common.sh
@@ -27,11 +27,63 @@ function tolower()
   echo "$1" | awk '{print tolower($0)}'
 }
 
+function toupper()
+{
+  echo "$1" | awk '{print toupper($0)}'
+}
+
 function print_banner()
 {
   local var="$1"
   local banner_text=`get_make_var banner_text scripts/banner-variables.mk`
   printf "*** ${var} Utility for ${banner_text} ***\n\n" 
+}
+
+printarr() {
+  local download_type="$1_DOWNLOAD_TYPE"
+  printf "%s=\"%s\"\n" "${download_type}" "${!download_type}"
+  local ver="$1_VER"
+  printf "%s=\"%s\"\n" "${ver}" "${!ver}"
+  local git_repo="$1_GIT_REPO"
+  printf "%s=\"%s\"\n" "${git_repo}" "${!git_repo}"
+  local git_branch="$1_GIT_BRANCH"
+  printf "%s=\"%s\"\n" "${git_branch}" "${!git_branch}"
+  local tarball_type="$1_TARBALL_TYPE"
+  printf "%s=\"%s\"\n" "${tarball_type}" "${!tarball_type}"
+  local tarball_url="$1_TARBALL_URL"
+  printf "%s=\"%s\"\n" "${tarball_url}" "${!tarball_url}"
+  printf "\n"
+}
+
+
+
+function setup_download_var()
+{
+  echo "Setup Download Var $1"
+
+  # Common Config Options
+  export $1_DOWNLOAD_TYPE=`get_make_var $(tolower $1)_download_type`
+  export $1_VER=`get_make_var $(tolower $1)_ver`
+  
+  # Git Repo Config Options
+  export $1_GIT_REPO=`get_make_var $(tolower $1)_git_repo`
+  export $1_GIT_BRANCH=`get_make_var $(tolower $1)_git_branch`
+
+  # Tarball Releases
+  export $1_TARBALL_TYPE=`get_make_var $(tolower $1)_tarball_type`
+
+  # asdf
+  local download_type="$1_DOWNLOAD_TYPE"
+  if [ -z ${!download_type} ]; then
+    export $1_DOWNLOAD_TYPE="tarball"
+  fi
+
+  printf "%s\n" "${!download_type}"
+  if [ "${!download_type}" != "git" ] && [ "${!download_type}" != "tarball" ]; then
+    echo >&2 "${!download_type} is an invalid option for $(tolower $1)_download_type" || exit 1
+  fi
+
+  printarr "$1"
 }
 
 # Check for config.mk
@@ -42,60 +94,64 @@ fi
   
 export CONFIG_GUESS="config.guess"
 
-export SH_BINUTILS_VER=`get_make_var sh_binutils_ver`
-export SH_GCC_VER=`get_make_var sh_gcc_ver`
-export NEWLIB_VER=`get_make_var newlib_ver`
-export GDB_VER=`get_make_var gdb_ver`
-export INSIGHT_VER=`get_make_var insight_ver`
-export ARM_BINUTILS_VER=`get_make_var arm_binutils_ver`
-export ARM_GCC_VER=`get_make_var arm_gcc_ver`
+setup_download_var SH_BINUTILS
+setup_download_var SH_GCC
+setup_download_var NEWLIB
+setup_download_var GDB
+setup_download_var INSIGHT
+setup_download_var ARM_BINUTILS
+setup_download_var ARM_GCC
 
-export SH_BINUTILS_TARBALL_TYPE=`get_make_var sh_binutils_tarball_type`
-export SH_GCC_TARBALL_TYPE=`get_make_var sh_gcc_tarball_type`
-export NEWLIB_TARBALL_TYPE=`get_make_var newlib_tarball_type`
-export GDB_TARBALL_TYPE=`get_make_var gdb_tarball_type`
-export INSIGHT_TARBALL_TYPE=`get_make_var insight_tarball_type`
-export ARM_BINUTILS_TARBALL_TYPE=`get_make_var arm_binutils_tarball_type`
-export ARM_GCC_TARBALL_TYPE=`get_make_var arm_gcc_tarball_type`
 
-export SH_BINUTILS_URL=ftp.gnu.org/gnu/binutils/binutils-${SH_BINUTILS_VER}.tar.${SH_BINUTILS_TARBALL_TYPE}
-export SH_GCC_URL=ftp.gnu.org/gnu/gcc/gcc-${SH_GCC_VER}/gcc-${SH_GCC_VER}.tar.${SH_GCC_TARBALL_TYPE}
-export NEWLIB_URL=sourceware.org/pub/newlib/newlib-${NEWLIB_VER}.tar.${NEWLIB_TARBALL_TYPE}
-export GDB_URL=ftp.gnu.org/gnu/gdb/gdb-${GDB_VER}.tar.${GDB_TARBALL_TYPE}
-export INSIGHT_URL=mirrors.kernel.org/sourceware/insight/releases/insight-${INSIGHT_VER}a.tar.${INSIGHT_TARBALL_TYPE}
-export ARM_BINUTILS_URL=ftp.gnu.org/gnu/binutils/binutils-${ARM_BINUTILS_VER}.tar.${ARM_BINUTILS_TARBALL_TYPE}
-export ARM_GCC_URL=ftp.gnu.org/gnu/gcc/gcc-${ARM_GCC_VER}/gcc-${ARM_GCC_VER}.tar.${ARM_GCC_TARBALL_TYPE}
+export KERNEL_MIRROR_URL=mirrors.kernel.org/gnu
+export GNU_MIRROR_URL=ftp.gnu.org
+export SH_BINUTILS_TARBALL_URL=$GNU_MIRROR_URL/gnu/binutils/binutils-$SH_BINUTILS_VER.tar.$SH_BINUTILS_TARBALL_TYPE
+export SH_GCC_TARBALL_URL=$GNU_MIRROR_URL/gnu/gcc/gcc-$SH_GCC_VER/gcc-$SH_GCC_VER.tar.$SH_GCC_TARBALL_TYPE
+export NEWLIB_TARBALL_URL=sourceware.org/pub/newlib/newlib-$NEWLIB_VER.tar.$NEWLIB_TARBALL_TYPE
+export GDB_TARBALL_URL=$GNU_MIRROR_URL/gnu/gdb/gdb-$GDB_VER.tar.$GDB_TARBALL_TYPE
+export INSIGHT_TARBALL_URL=$GNU_MIRROR_URL/sourceware/insight/releases/insight-$INSIGHT_VER.tar.$INSIGHT_TARBALL_TYPE
+export ARM_BINUTILS_TARBALL_URL=$GNU_MIRROR_URL/gnu/binutils/binutils-$ARM_BINUTILS_VER.tar.$ARM_BINUTILS_TARBALL_TYPE
+export ARM_GCC_TARBALL_URL=$GNU_MIRROR_URL/gnu/gcc/gcc-$ARM_GCC_VER/gcc-$ARM_GCC_VER.tar.$ARM_GCC_TARBALL_TYPE
+
+printarr SH_BINUTILS
+printarr SH_GCC
+printarr NEWLIB
+printarr GDB
+printarr INSIGHT
+printarr ARM_BINUTILS
+printarr ARM_GCC
+
 
 export DOWNLOAD_PROTOCOL="`get_make_var download_protocol`://"
 
 export USE_CUSTOM_DEPENDENCIES=`get_make_var use_custom_dependencies`
 
-export SH_GMP_VER=`get_make_var sh_gmp_ver`
-export SH_MPFR_VER=`get_make_var sh_mpfr_ver`
-export SH_MPC_VER=`get_make_var sh_mpc_ver`
-export SH_ISL_VER=`get_make_var sh_isl_ver`
-export ARM_GMP_VER=`get_make_var arm_gmp_ver`
-export ARM_MPFR_VER=`get_make_var arm_mpfr_ver`
-export ARM_MPC_VER=`get_make_var arm_mpc_ver`
-export ARM_ISL_VER=`get_make_var arm_isl_ver`
+setup_download_var SH_GMP
+setup_download_var SH_MPFR
+setup_download_var SH_MPC
+setup_download_var SH_ISL
+setup_download_var ARM_GMP
+setup_download_var ARM_MPFR
+setup_download_var ARM_MPC
+setup_download_var ARM_ISL
 
-export SH_GMP_TARBALL_TYPE=`get_make_var sh_gmp_tarball_type`
-export SH_MPFR_TARBALL_TYPE=`get_make_var sh_mpfr_tarball_type`
-export SH_MPC_TARBALL_TYPE=`get_make_var sh_mpc_tarball_type`
-export SH_ISL_TARBALL_TYPE=`get_make_var sh_isl_tarball_type`
-export ARM_GMP_TARBALL_TYPE=`get_make_var arm_gmp_tarball_type`
-export ARM_MPFR_TARBALL_TYPE=`get_make_var arm_mpfr_tarball_type`
-export ARM_MPC_TARBALL_TYPE=`get_make_var arm_mpc_tarball_type`
-export ARM_ISL_TARBALL_TYPE=`get_make_var arm_isl_tarball_type`
+export SH_GMP_TARBALL_URL=gcc.gnu.org/pub/gcc/infrastructure/gmp-${SH_GMP_VER}.tar.${SH_GMP_TARBALL_TYPE}
+export SH_MPFR_TARBALL_URL=gcc.gnu.org/pub/gcc/infrastructure/mpfr-${SH_MPFR_VER}.tar.${SH_MPFR_TARBALL_TYPE}
+export SH_MPC_TARBALL_URL=gcc.gnu.org/pub/gcc/infrastructure/mpc-${SH_MPC_VER}.tar.${SH_MPC_TARBALL_TYPE}
+export SH_ISL_TARBALL_URL=gcc.gnu.org/pub/gcc/infrastructure/isl-${SH_ISL_VER}.tar.${SH_ISL_TARBALL_TYPE}
+export ARM_GMP_TARBALL_URL=gcc.gnu.org/pub/gcc/infrastructure/gmp-${ARM_GMP_VER}.tar.${ARM_GMP_TARBALL_TYPE}
+export ARM_MPFR_TARBALL_URL=gcc.gnu.org/pub/gcc/infrastructure/mpfr-${ARM_MPFR_VER}.tar.${ARM_MPFR_TARBALL_TYPE}
+export ARM_MPC_TARBALL_URL=gcc.gnu.org/pub/gcc/infrastructure/mpc-${ARM_MPC_VER}.tar.${ARM_MPC_TARBALL_TYPE}
+export ARM_ISL_TARBALL_URL=gcc.gnu.org/pub/gcc/infrastructure/isl-${ARM_ISL_VER}.tar.${ARM_ISL_TARBALL_TYPE}
 
-export SH_GMP_URL=gcc.gnu.org/pub/gcc/infrastructure/gmp-${SH_GMP_VER}.tar.${SH_GMP_TARBALL_TYPE}
-export SH_MPFR_URL=gcc.gnu.org/pub/gcc/infrastructure/mpfr-${SH_MPFR_VER}.tar.${SH_MPFR_TARBALL_TYPE}
-export SH_MPC_URL=gcc.gnu.org/pub/gcc/infrastructure/mpc-${SH_MPC_VER}.tar.${SH_MPC_TARBALL_TYPE}
-export SH_ISL_URL=gcc.gnu.org/pub/gcc/infrastructure/isl-${SH_ISL_VER}.tar.${SH_ISL_TARBALL_TYPE}
-export ARM_GMP_URL=gcc.gnu.org/pub/gcc/infrastructure/gmp-${ARM_GMP_VER}.tar.${ARM_GMP_TARBALL_TYPE}
-export ARM_MPFR_URL=gcc.gnu.org/pub/gcc/infrastructure/mpfr-${ARM_MPFR_VER}.tar.${ARM_MPFR_TARBALL_TYPE}
-export ARM_MPC_URL=gcc.gnu.org/pub/gcc/infrastructure/mpc-${ARM_MPC_VER}.tar.${ARM_MPC_TARBALL_TYPE}
-export ARM_ISL_URL=gcc.gnu.org/pub/gcc/infrastructure/isl-${ARM_ISL_VER}.tar.${ARM_ISL_TARBALL_TYPE}
+printarr SH_GMP
+printarr SH_MPFR
+printarr SH_MPC
+printarr SH_ISL
+printarr ARM_GMP
+printarr ARM_MPFR
+printarr ARM_MPC
+printarr ARM_ISL
 
 # Retrieve the web downloader program available in this system.
 export IS_CURL=0
